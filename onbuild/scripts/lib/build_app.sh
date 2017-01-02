058aa1487b18ee210844fcada349c4cdbe4f6359
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2017-01-02 12:39:59 -0300

    update onbuild scripts

--- onbuild/scripts/lib/build_app.sh
+++ onbuild/scripts/lib/build_app.sh
@@ -1,24 +1,23 @@
 set -e
 
-if [[ ! $DEVELOPMENT ]]; then
-	COPIED_APP_PATH=/copied-app
-	BUNDLE_DIR=/tmp/bundle-dir
-	# sometimes, directly copied folder cause some wierd issues
-	# this fixes that
-	cp -R /app $COPIED_APP_PATH
-	cd $COPIED_APP_PATH
+COPIED_APP_PATH=/copied-app
+BUNDLE_DIR=/tmp/bundle-dir
+# sometimes, directly copied folder cause some wierd issues
+# this fixes that
+cp -R /app $COPIED_APP_PATH
+cd $COPIED_APP_PATH
 
-	meteor npm i --production
-	meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
+meteor npm i --production
+meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
-	cd $BUNDLE_DIR/bundle/programs/server/
-	npm i --production
+cd $BUNDLE_DIR/bundle/programs/server/
+npm i --production
 
-	mv $BUNDLE_DIR/bundle /built_app
+mkdir -p /built_app
+rsync -rlcv --delete $BUNDLE_DIR/bundle /built_app
 
-	# cleanup
-	rm -rf $COPIED_APP_PATH
-	rm -rf $BUNDLE_DIR
-	rm -rf ~/.meteor
-	rm /usr/local/bin/meteor
-fi
+# cleanup
+rm -rf $COPIED_APP_PATH
+rm -rf $BUNDLE_DIR
+rm -rf ~/.meteor
+rm /usr/local/bin/meteor
