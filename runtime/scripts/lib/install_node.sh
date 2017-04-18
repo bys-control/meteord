bcddd22c2d233eb02e2af2efadaa36fc3a316ae6
Guillermo Bisheimer <gbisheimer@bys-control.com.ar>
2017-04-18 17:16:33 -0300

    update runtime images

--- runtime/scripts/lib/install_node.sh
+++ runtime/scripts/lib/install_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-NODE_VERSION=$(</built_app/bundle/.node_version.txt)
+NODE_VERSION=$(</built_app/.node_version.txt)
 NODE_ARCH=x64
 NODE_DIST=node-${NODE_VERSION}-linux-${NODE_ARCH}
 
